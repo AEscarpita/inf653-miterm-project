51b934cad8d7fbbce08d54abdb434f1f71545fae
Arnulfo Escarpita <a_escarpita@mail.fhsu.edu>
2025-03-20 15:36:09 -0500

    create debug cont. 3

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -35,7 +35,6 @@
         }
 
     }catch(PDOException $e){
-        echo json_encode(array('Message' => $e->getMessage()));
         if(strpos($e->getMessage(), 'qoutes_author_id_key')){
             echo json_encode(array('Message' => "author_id Not Found"));
         }elseif(strpos($e->getMessage(), 'qoutes_category_id_key')){
